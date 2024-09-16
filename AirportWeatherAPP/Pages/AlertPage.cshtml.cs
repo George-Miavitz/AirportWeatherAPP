b56de3c0f28c5ae7a429c0e0ac5b4ea9136d52f7
George-Miavitz <156019347+George-Miavitz@users.noreply.github.com>
2024-09-16 18:58:28 -0400

    New Pages Added-Taylor

---
+++ AirportWeatherAPP/Pages/AlertPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AirportWeatherAPP.Pages
+{
+    public class AlertPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
