c2d8dd8d0eaad0dde9d1d0d87d9a3715e44f7b87
George-Miavitz <156019347+George-Miavitz@users.noreply.github.com>
2024-10-28 22:13:15 -0400

    HW 4 In Progress_NotComplete(ManyErrors)

---
+++ AirportWeatherAppAPI/Data/DbContextClass.cs
@@ -0,0 +1,19 @@
+﻿using AirportWeatherAppAPI.Data;
+using Microsoft.EntityFrameworkCore;
+namespace AirportWeatherAppAPI.Data
+{
+    public class DbContextClass : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public DbContextClass(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+        public DbSet<User> User { get; set; }
+     
+    }
+}
---
+++ AirportWeatherAppAPI/TaylorSPRepositories/UserEmailUpdate.cs
@@ -0,0 +1,26 @@
+﻿using AirportWeatherAppAPI.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AirportWeatherAppAPI.Repositories
+{
+    public class NewEmail : IUserEmailUpdate
+    {
+        private readonly DbContextClass _dbcontext;
+
+        public NewEmail(DbContextClass dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task<int> UpdateEmail(User NewEmail)
+        {
+            var parameter = new List<SqlParameter>();
+            parameter.Add(new SqlParameter("@UserID", User.UserId));,
+            parameter.Add(new SqlParameter("@NewEmail", User.Email));
+            return await _dbcontext.Database.ExecuteSqlRawAsync("exec spUserEmailUpdate @UserID, @NewEmail", parameter.ToArray());
+        }
+
+    }
+}
