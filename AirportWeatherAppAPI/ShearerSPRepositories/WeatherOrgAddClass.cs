e8be8af1a9fc69c96b74854084ef0a3e83216512
George-Miavitz <156019347+George-Miavitz@users.noreply.github.com>
2024-10-31 16:08:59 -0400

    Final Push for individual cont. -Taylor (No Errors)

--- AirportWeatherAppAPI/ShearerSPRepositories/WeatherOrgAddClass.cs
+++ AirportWeatherAppAPI/ShearerSPRepositories/WeatherOrgAddClass.cs
@@ -1,21 +1,21 @@
 ﻿using AirportWeatherAppAPI.Data;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
+//using Microsoft.Data.SqlClient;
+//using Microsoft.EntityFrameworkCore;
 
-namespace AirportWeatherAppAPI.ShearerSPRepositories
-{
-    public class WeatherOrgAddClass : IWeatherOrgAddClass
-    {
-        private readonly DbContextClass _dbContext;
-        public WeatherOrgAddClass(DbContextClass dbContext)
-        {
-            _dbContext = dbContext;
-        }
-        public async Task<int> WeatherOrgAdd(WeatherDataOrg OrgId)
-        {
-            var parameter = new List<SqlParameter>();
-            parameter.Add(new SqlParameter("@OrgName", OrgId.OrgName));
-            return await _dbContext.Database.ExecuteSqlRawAsync("exec spWeatherOrgAdd @OrgID", parameter);
-        }
-    }
-}
+//namespace AirportWeatherAppAPI.ShearerSPRepositories
+//{
+//    public class WeatherOrgAddClass : 
+//    {
+//        private readonly DbContextClass _dbContext;
+//        public WeatherOrgAddClass(DbContextClass dbContext)
+//        {
+//            _dbContext = dbContext;
+//        }
+//        public async Task<int> WeatherOrgAdd(WeatherDataOrg OrgId)
+//        {
+//            var parameter = new List<SqlParameter>();
+//            parameter.Add(new SqlParameter("@OrgName", OrgId.OrgName));
+//            return await _dbContext.Database.ExecuteSqlRawAsync("exec spWeatherOrgAdd @OrgID", parameter);
+//        }
+//    }
+//}
