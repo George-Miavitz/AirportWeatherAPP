e546aca2ff366c1ea75975f10d77662757e65d17
Phillip Waller <prw00007@mail.wvu.edu>
2024-11-12 15:33:23 -0500

    Phil CRUD

---
+++ AirportWeatherAPP/Pages/OrganizationCRUD/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AirportWeatherAPP.Data;
+
+namespace AirportWeatherAPP.Pages.OrganizationCRUD
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly AirportWeatherAPP.Data.AirportWeatherAppContext _context;
+
+        public DeleteModel(AirportWeatherAPP.Data.AirportWeatherAppContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public WeatherDataOrg WeatherDataOrg { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var weatherdataorg = await _context.WeatherDataOrgs.FirstOrDefaultAsync(m => m.OrgId == id);
+
+            if (weatherdataorg == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                WeatherDataOrg = weatherdataorg;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var weatherdataorg = await _context.WeatherDataOrgs.FindAsync(id);
+            if (weatherdataorg != null)
+            {
+                WeatherDataOrg = weatherdataorg;
+                _context.WeatherDataOrgs.Remove(WeatherDataOrg);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ AirportWeatherAPP/Pages/OrganizationCRUD/Edit.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AirportWeatherAPP.Data;
+
+namespace AirportWeatherAPP.Pages.OrganizationCRUD
+{
+    public class EditModel : PageModel
+    {
+        private readonly AirportWeatherAPP.Data.AirportWeatherAppContext _context;
+
+        public EditModel(AirportWeatherAPP.Data.AirportWeatherAppContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public WeatherDataOrg WeatherDataOrg { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var weatherdataorg =  await _context.WeatherDataOrgs.FirstOrDefaultAsync(m => m.OrgId == id);
+            if (weatherdataorg == null)
+            {
+                return NotFound();
+            }
+            WeatherDataOrg = weatherdataorg;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(WeatherDataOrg).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WeatherDataOrgExists(WeatherDataOrg.OrgId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool WeatherDataOrgExists(int id)
+        {
+            return _context.WeatherDataOrgs.Any(e => e.OrgId == id);
+        }
+    }
+}
