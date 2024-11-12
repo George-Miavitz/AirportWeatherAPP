9cc8cc128fa242136ee881bb17ff93d56b36be03
DiegoReyes4 <dr00033@mix.wvu.edu>
2024-11-12 14:40:31 -0500

    Diego Started on Alert CRUD

---
+++ AirportWeatherAPP/Pages/AlertCRUD/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AirportWeatherAPP.Data;
+
+namespace AirportWeatherAPP.Pages.AlertCRUD
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AirportWeatherAPP.Data.AirportWeatherAppContext _context;
+
+        public CreateModel(AirportWeatherAPP.Data.AirportWeatherAppContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["ObservationId"] = new SelectList(_context.Observations, "ObservationId", "ObservationId");
+        ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return Page();
+        }
+
+        [BindProperty]
+        public Alert Alert { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Alerts.Add(Alert);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ AirportWeatherAPP/Pages/AlertCRUD/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AirportWeatherAPP.Data;
+
+namespace AirportWeatherAPP.Pages.AlertCRUD
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly AirportWeatherAPP.Data.AirportWeatherAppContext _context;
+
+        public DeleteModel(AirportWeatherAPP.Data.AirportWeatherAppContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Alert Alert { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var alert = await _context.Alerts.FirstOrDefaultAsync(m => m.AlertId == id);
+
+            if (alert == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Alert = alert;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var alert = await _context.Alerts.FindAsync(id);
+            if (alert != null)
+            {
+                Alert = alert;
+                _context.Alerts.Remove(Alert);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
